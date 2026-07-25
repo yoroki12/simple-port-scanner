cbde86bca0fa551055630a08d4f5f46ae8f32a48
yoroki12 <kevinjonathanhendrawan@gmail.com>
2026-07-25 20:08:37 +0700

    progress bar and IP identifier

--- SimplePortScanner.py
+++ SimplePortScanner.py
@@ -1,9 +1,28 @@
 import socket
+import ipaddress
 from concurrent.futures import ThreadPoolExecutor
 from datetime import datetime
+from tqdm import tqdm
 
+check = 0
 open_ports = []
-target = input("Target IP/domain: ")
+
+
+while True:
+    target = input("Target IP/domain: ")
+    
+    try: 
+        ipaddress.ip_address(target)
+        ip = target
+        check += 1
+        break
+    except ValueError:
+        try:
+            ip = socket.gethostbyname(target)
+            break
+        except socket.gaierror:
+            print ("Invalid ip or domain")
+
 while True:
     max = int(input("Max port to scan(1-65535): "))
     if max < 1 or max > 65535:
@@ -14,7 +33,7 @@ while True:
 def scan_port(port):
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     sock.settimeout(1)
-    result = sock.connect_ex((target, port))
+    result = sock.connect_ex((ip, port))
     
     if result == 0:
         try:
@@ -23,15 +42,19 @@ def scan_port(port):
             service = "unknown"
         
         open_ports.append((port, service))
-        print(f"Port {port}: OPEN ({service})")
-    
+        tqdm.write(f"Port {port}: OPEN ({service})")    
     sock.close()
 
 start_time = datetime.now()
 print(f"start scan: {start_time}")
 
 with ThreadPoolExecutor(max_workers=50) as executor:
-    executor.map(scan_port, range(1, max+1))
+    list(tqdm(
+        executor.map(scan_port, range(1, max + 1)),
+        total=max,
+        desc="Scanning Ports",
+        unit="port"
+    ))
 
 end_time = datetime.now()
 open_ports.sort() 
@@ -46,5 +69,10 @@ with open(filename, "w") as f:
     f.write(f"\nOpen ports:\n")
     for port, service in open_ports:
         f.write(f"  Port {port}: {service}\n")
+    f.write(f"\nTotal open ports: {len(open_ports)}\n")
+    if check == 0:
+        f.write(f"IP Address: {ip}\n")
+    else:
+        pass
 
 print(f"\nscan completed! Results saved to {filename}")
